d7d245eb1fe7670e4be612ccfc17d88718713f88
Jack Agtual <jaagtual@gmail.com>
2022-09-19 18:17:06 -0700

    Adding operate function

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ const add      = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide   = (a, b) => a / b;
+
+const operate = (operator, a, b) => {
+    if      (operator === '+') return add(a, b);
+    else if (operator === '-') return subtract(a, b);
+    else if (operator === '*') return multiply(a, b);
+    else if (operator === '/') return divide(a, b);
+    else {
+        console.error('Invalid operator');
+    }
+}
