2d6541b45df93560eb8b99b61388edb67d2f5bb5
Jack Agtual <jaagtual@gmail.com>
2022-09-30 12:47:05 -0700

    Refactoring: DRYing code and reordering

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-const add      = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide   = (a, b) => a / b;
-const operations = ['+', '-', '*', '/'];
-
 // state variables
 let started = false;
 let operationState;
@@ -12,6 +6,14 @@ let prevBtn;
 let prevOpBtn;
 const opBtnDefaultColor = 'rgb(238, 169, 21)';
 const opBtnTargetColor = 'rgb(241, 246, 251)';
+
+const operations = ['+', '-', '*', '/'];
+
+// Helper functions
+const add      = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide   = (a, b) => a / b;
 
 const operate = (operator, a, b) => {
     let ans;
@@ -24,7 +26,61 @@ const operate = (operator, a, b) => {
     }
 
     return Math.round(ans * 100) / 100;
+};
+
+const isValidEval = evalStr => {
+    // check if evaluation is valid
+    let invalidEnds = [...operations, '.'];
+    let okStart = ['+', '-', '.']; // evalStr can start with these chars
+
+    // starts or ends with invalid char
+    for (let i = 0; i < invalidEnds.length; i++) {
+        if ((evalStr.startsWith(invalidEnds[i]) && !okStart.includes(invalidEnds[i])) || 
+            evalStr.endsWith(invalidEnds[i])) return false;
+    }
+
+    // two invalid chars next to each other
+    for (let i = 1; i < evalStr.length; i++) {
+        if (invalidEnds.includes(evalStr.charAt(i)) && 
+            invalidEnds.includes(evalStr.charAt(i-1))) return false;
+    }
+
+    // contains an operation (can't be first +/-)
+    let hasOp = false;
+    const startIdx = okStart.includes(evalStr.charAt(0)) ? 1 : 0;
+    for (let i = startIdx; i < evalStr.length; i++) {
+        if (operations.includes(evalStr.charAt(i))) {
+            hasOp = true;
+            break;
+        }
+    }
+    if (!hasOp) return false;
+    
+    return true;
+};
+
+const evalExpression = (evalStr, operationState) => {
+    
+    // operation state is defined (not first time clicking operation)
+    if (operationState) return operate(operationState, prevNum, Number(screen.innerText));
+
+    // get index of operation
+    let opIdx;
+    for (let i = 0; i < evalStr.length; i++) {
+        if (operations.includes(evalStr.charAt(i))){
+            opIdx = i;
+            break;
+        }
+    }
+    
+    // evaluate expression
+    let op = evalStr.charAt(opIdx);
+    let num1 = Number(evalStr.substring(0, opIdx));
+    let num2 = Number(evalStr.substring(opIdx + 1));
+    
+    return operate(op, num1, num2);
 }
+// End helper functions
 
 // add clicked buttons to screen
 const screen = document.querySelector('.screen');
@@ -66,61 +122,7 @@ deleteBtn.addEventListener('click', () => {
     screen.innerText = newStr;
 });
 
-// check if evaluation is valid
-const isValidEval = evalStr => {
-    let invalidEnds = [...operations, '.'];
-    let okStart = ['+', '-', '.']; // evalStr can start with these chars
-
-    // starts or ends with invalid char
-    for (let i = 0; i < invalidEnds.length; i++) {
-        if ((evalStr.startsWith(invalidEnds[i]) && !okStart.includes(invalidEnds[i])) || 
-            evalStr.endsWith(invalidEnds[i])) return false;
-    }
-
-    // two invalid chars next to each other
-    for (let i = 1; i < evalStr.length; i++) {
-        if (invalidEnds.includes(evalStr.charAt(i)) && 
-            invalidEnds.includes(evalStr.charAt(i-1))) return false;
-    }
-
-    // contains an operation (can't be first +/-)
-    let hasOp = false;
-    const startIdx = okStart.includes(evalStr.charAt(0)) ? 1 : 0;
-    for (let i = startIdx; i < evalStr.length; i++) {
-        if (operations.includes(evalStr.charAt(i))) {
-            hasOp = true;
-            break;
-        }
-    }
-    if (!hasOp) return false;
-    
-    return true;
-}
-
-
 // equal btn 
-const evalExpression = (evalStr, operationState) => {
-    
-    // operation state is defined (not first time clicking operation)
-    if (operationState) return operate(operationState, prevNum, Number(screen.innerText));
-
-    // get index of operation
-    let opIdx;
-    for (let i = 0; i < evalStr.length; i++) {
-        if (operations.includes(evalStr.charAt(i))){
-            opIdx = i;
-            break;
-        }
-    }
-    
-    // evaluate expression
-    let op = evalStr.charAt(opIdx);
-    let num1 = Number(evalStr.substring(0, opIdx));
-    let num2 = Number(evalStr.substring(opIdx + 1));
-    
-    return operate(op, num1, num2);
-}
-
 const equalBtn = document.querySelector('#equals');
 equalBtn.addEventListener('click', () => {
     const evalStr = screen.innerText;
@@ -164,11 +166,12 @@ opBtns.forEach(opBtn => {
         operationState = opBtn.innerText;
         prevNum = ans;
     });
-})
+});
 
+// Save previous btn
 const allBtns = document.querySelectorAll('button');
 allBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         prevBtn = btn.innerText;
-    })
-})
+    });
+});
